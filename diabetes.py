12902448debbd7331e594dd3bb48dedf35600afb
M.Shaheer Nadeem <160414159+M-Shaheer-Texra@users.noreply.github.com>
2024-05-03 09:57:42 +0500

    Create diabetes.py
    dividing code into small chunks

---
+++ diabetes.py
@@ -0,0 +1,33 @@
+print(end="\n")
+print("Diabetes Dataset")
+for i in range(50):
+    print("--", end="")
+print()
+
+
+diabetes_data = datasets.load_diabetes()
+
+X_diabetes = diabetes_data.data
+y_diabetes = diabetes_data.target
+
+X_diabetes_train, X_diabetes_test, y_diabetes_train, y_diabetes_test = train_test_split(X_diabetes, y_diabetes, test_size=0.3, random_state=32)
+
+decision_tree_model.fit(X_diabetes_train, y_diabetes_train)
+
+y_diabetes_pred = decision_tree_model.predict(X_diabetes_test)
+
+print("Accuracy score For diabetes using Decision Tree:",accuracy_score(y_diabetes_test, y_diabetes_pred))
+
+k_neighbors_model.fit(X_diabetes_train, y_diabetes_train)
+
+y_diabetes_pred = k_neighbors_model.predict(X_diabetes_test)
+
+print("Accuracy score For diabetes using K-Neighbors:",accuracy_score(y_diabetes_test, y_diabetes_pred))
+
+naive_bayes_model = GaussianNB()
+
+naive_bayes_model.fit(X_diabetes_train, y_diabetes_train)
+
+y_diabetes_pred = naive_bayes_model.predict(X_diabetes_test)
+
+print("Accuracy score For diabetes using Naive Bayes:",accuracy_score(y_diabetes_test, y_diabetes_pred))
