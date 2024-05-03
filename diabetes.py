eadf48193ba1d2d0cc63bc505a4bc9c15b543d5b
M.Shaheer Nadeem <160414159+M-Shaheer-Texra@users.noreply.github.com>
2024-05-03 09:58:19 +0500

    Update diabetes.py
    adding necessary libraries and functions

--- diabetes.py
+++ diabetes.py
@@ -1,3 +1,16 @@
+import pandas as pd
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder, OneHotEncoder
+
+
 print(end="\n")
 print("Diabetes Dataset")
 for i in range(50):
