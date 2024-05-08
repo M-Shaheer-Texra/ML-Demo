2a18622a54e98e50a932253f553cb34644e39845
M.Shaheer Nadeem <160414159+M-Shaheer-Texra@users.noreply.github.com>
2024-05-08 18:36:22 +0500

    performed image loading task and general clean-up

--- Mnist/mnistRAW.py
+++ Mnist/mnistRAW.py
@@ -6,7 +6,8 @@ from PIL import Image
 import tensorflow as tf
 from keras import layers
 from keras import Sequential
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+from sklearn.model_selection import train_test_split
 
 def load_training_images_from_folder(folder):
     images = []
@@ -34,14 +35,19 @@ def load_testing_images_from_folder(folder):
             labels.append(random.randint(0, 9))
     return np.array(images), np.array(labels)
 
-training_folder = "C:/Users/Nadeem/Desktop/ML-Demo/data/mnistdata/trainingSamplecls"
+training_folder = "C:/Users/Nadeem/Desktop/ML-Demo/data/mnistdata/trainingSet/trainingSet"
 test_folder = "C:/Users/Nadeem/Desktop/ML-Demo/data/mnistdata/testSamplecls"
 
-train_images, train_labels = load_training_images_from_folder(training_folder)
-test_images, test_labels = load_testing_images_from_folder(test_folder)
+images, labels = load_training_images_from_folder(training_folder)
+images = images.astype('float32') / 255.0
+print(images.shape, labels.shape)
 
-train_images = train_images.astype('float32') / 255.0
-test_images = test_images.astype('float32') / 255.0
+X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.3, random_state=2)
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+# test_images = test_images.astype('float32') / 255.0#
 
 #Designing a random CNN:
 model = Sequential()
@@ -58,24 +64,24 @@ model.add(layers.Dense(10, activation='softmax'))
 
 #Model Summary
 model.summary()
-
+#
 #Compiling and fitting model
 model.compile(
     loss='sparse_categorical_crossentropy',
     optimizer= keras.optimizers.Adam(learning_rate=1e-3),
     metrics=['accuracy']
 )
-model.fit(train_images, train_labels, epochs=15, batch_size=32)
-
+model.fit(X_train, y_train, epochs=15, batch_size=32)
+#
 #Pridiction
-y_hat = model.predict(test_images)
+y_hat = model.predict(X_test)
 y_hat = tf.nn.softmax(y_hat).numpy()
 y_hat = np.argmax(y_hat, axis=1)
-print(model.evaluate(test_images, test_labels))
-
-
+(print(accuracy_score(y_hat, y_test)))
+#
+#
 #Evaluation
-precision, recall, fscore, support = precision_recall_fscore_support(test_labels, y_hat, average='micro')
+precision, recall, fscore, support = precision_recall_fscore_support(y_test, y_hat, average='micro')
 print("Precision:", precision)
 print("Recall:", recall)
 # Calculate TPR (Recall) for each class
--- Mnist/mnistRAW_c.py
+++ Mnist/mnistRAW_c.py
@@ -1,101 +1,88 @@
-import os
-import random
+from PIL import Image
 import keras
 import numpy as np
-from PIL import Image
 import tensorflow as tf
 from keras import layers
 from keras import Sequential
-from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
 
-train_set = keras.utils.image_dataset_from_directory(
-    directory="C:/Users/Nadeem/Desktop/ML-Demo/data/mnistdata/trainingSet/trainingSet",
+
+data_set_path = "C:/Users/Nadeem/Desktop/ML-Demo/data/mnistdata/trainingSet/trainingSet"
+
+train_data, test_data = keras.utils.image_dataset_from_directory(
+    directory=data_set_path,
     labels="inferred",
     label_mode="categorical",
-    batch_size=32,
+    shuffle=True,
+    seed=123,
+    validation_split=0.2,
+    subset="both",
     image_size=(28, 28),
+    batch_size=32,
 )
 
-test_set = keras.utils.image_dataset_from_directory(
-    directory="C:/Users/Nadeem/Desktop/ML-Demo/data/mnistdata/testSet",
-    labels="inferred",
-    label_mode="categorical",
-    batch_size=32,
-    image_size=(28, 28),
-)
+x, y = zip(*train_data)
+X_train = np.concatenate(x)
+y_train = np.concatenate(y)
+x, y = zip(*test_data)
+X_test = np.concatenate(x)
+y_test = np.concatenate(y)
 
-print(test_set.class_names)
+#write a lambda function for average for y_train and y_test
+y_train = y_train.argmax(axis=1)
+y_test = y_test.argmax(axis=1)
 
-# def load_training_images_from_folder(folder):
-#     images = []
-#     labels = []
-#     for class_folder in os.listdir(folder):
-#         class_folder_path = os.path.join(folder, class_folder)
-#         if os.path.isdir(class_folder_path):
-#             label = int(class_folder)
-#             for file_name in os.listdir(class_folder_path):
-#                 img_path = os.path.join(class_folder_path, file_name)
-#                 with Image.open(img_path) as img:
-#                     img_array = np.array(img)
-#                     images.append(img_array)
-#                     labels.append(label)
-#     return np.array(images), np.array(labels)
-#
-# def load_testing_images_from_folder(folder):
-#     images = []
-#     labels = []
-#     for file_name in os.listdir(folder):
-#         img_path = os.path.join(folder, file_name)
-#         with Image.open(img_path) as img:
-#             img_array = np.array(img)
-#             images.append(img_array)
-#             labels.append(random.randint(0, 9))
-#     return np.array(images), np.array(labels)
-#
-# training_folder = "C:/Users/Nadeem/Desktop/ML-Demo/data/mnistdata/trainingSet/trainingSet"
-# test_folder = "C:/Users/Nadeem/Desktop/ML-Demo/data/mnistdata/testSet/testSet"
-#
-# train_images, train_labels = load_training_images_from_folder(training_folder)
-# test_images, test_labels = load_testing_images_from_folder(test_folder)
-#
 
-#Designing a random CNN:
-model = Sequential()
-model.add(layers.Conv2D(6, kernel_size=(5,5), activation='relu', padding='same', strides=(1, 1), input_shape=(28, 28 ,1)))
-model.add(layers.MaxPooling2D(pool_size=(1,1), padding='valid', strides=(1,1)))
-model.add(layers.Conv2D(16, kernel_size=(5,5), activation='relu', padding='valid', strides=(2,2)))
-model.add(layers.AveragePooling2D(pool_size=(2,2), padding='valid', strides=(1,1)))
-model.add(layers.Conv2D(6, kernel_size=(5,5), activation='relu', padding='same', strides=(1, 1), input_shape=(28, 28 ,1)))
-model.add(layers.MaxPooling2D(pool_size=(2,2), padding='valid', strides=(1,1)))
-model.add(layers.Flatten())
-model.add(layers.Dense(120, activation='relu'))
-model.add(layers.Dense(84, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+#normalize these images
+X_train = X_train.astype('float32') / 255.0
+X_test = X_test.astype('float32') / 255.0
 
-#Model Summary
+class_names = np.array(train_data.class_names)
+print(class_names)
+
+my_input_shape = (28, 28, 3)
+num_classes = len(class_names)
+
+initial_lr = 0.0001
+
+model = keras.Sequential([
+    layers.InputLayer(my_input_shape),
+
+    # Convulutional Block
+    layers.Conv2D(filters=8, kernel_size=3, strides=2, padding='valid', activation='relu'),
+    layers.Conv2D(filters=16, kernel_size=3, strides=2, padding='same', activation='relu'),
+    layers.Conv2D(filters=32, kernel_size=3, strides=2, padding='same', activation='relu'),
+    layers.Flatten(),
+
+    # Fully Connected Block
+    layers.Dense(32, activation='relu'),
+    layers.Dense(16, activation='relu'),
+    layers.Dense(num_classes, activation='softmax')
+])
+
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=initial_lr),
+                     loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
-#Compiling and fitting model
-model.compile(
-    loss='sparse_categorical_crossentropy',
-    optimizer= keras.optimizers.Adam(learning_rate=1e-3),
-    metrics=['accuracy']
+model.fit(
+    train_data,
+    epochs=10,
+    batch_size=32
 )
-model.fit(train_set, epochs=15, batch_size=32)
-
 #Pridiction
-y_hat = model.predict(test_set)
+y_hat = model.predict(X_test)
 y_hat = tf.nn.softmax(y_hat).numpy()
 y_hat = np.argmax(y_hat, axis=1)
-print(model.evaluate(test_set, verbose=2))
+print(accuracy_score(y_hat, y_test))
+
 #
-#
-# #Evaluation
-# precision, recall, fscore, support = precision_recall_fscore_support(test_labels, y_hat, average='micro')
-# print("Precision:", precision)
-# print("Recall:", recall)
-# # Calculate TPR (Recall) for each class
-# TPR = recall
-# print("True Positive Rate OR sensitivity (Recall):", TPR)
-# FPR = 1 - precision
-# print("False Positive Rate OR specificity:", FPR)
+#Evaluation
+precision, recall, fscore, support = precision_recall_fscore_support(y_test, y_hat, average='micro')
+print("Precision:", precision)
+print("Recall:", recall)
+# Calculate TPR (Recall) for each class
+TPR = recall
+print("True Positive Rate OR sensitivity (Recall):", TPR)
+FPR = 1 - precision
+print("False Positive Rate OR specificity:", FPR)
